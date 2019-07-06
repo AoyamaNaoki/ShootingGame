05cdb390ecfc9c74eb0301d5afe20de6c4e71534
Naoki Aoyama <syrup3663ve@gmail.com>
2019-07-07 01:05:46 +0900

    プレイヤーシュートをリスト化してリストのリスト入力
    (最悪このコミットはdiscard)

--- Assets/Scripts/Shooter/PlayerShooter/PlayerShootActivator.cs
+++ Assets/Scripts/Shooter/PlayerShooter/PlayerShootActivator.cs
@@ -4,25 +4,36 @@ using UnityEngine;
 
 public class PlayerShootActivator : Shooter {
 
-    [SerializeField]private List<GameObject> childShooterList = new List<GameObject>();
+    [SerializeField] private List<GameObject> childShooterList = new List<GameObject>();
+    [SerializeField] private List<List<GameObject>> grandChildShooterList = new List<List<GameObject>>();
 
-    public void Update() {
-        if (Input.GetButtonDown("Roll")) {
-            SetDiceMainShoot();
-        }
-    }
+    public List<GameObject> ChildShooterList { get => childShooterList; set => childShooterList = value; }
+    public List<List<GameObject>> GrandChildShooterList { get => grandChildShooterList; set => grandChildShooterList = value; }
 
-    public  void SetDiceMainShoot() {
-        if(childShooterList.Count == 0) {
+    public void SetDiceMainShoot() {
+        if (ChildShooterList.Count == 0) {
             GetChildShooter();
+            GetGrandChildShooterList();
         }
-        childShooterList.ForEach(u => u.GetComponent<ParticlePlayer>().enabled = false);
-        childShooterList[Player.DiceNum - 1].GetComponent<ParticlePlayer>().enabled = true;
+        GrandChildShooterList.ForEach(GrandChildShooterList2 => {
+            GrandChildShooterList2.ForEach(grandChildShooter => { grandChildShooter.GetComponent<ParticlePlayer>().enabled = false; });
+        });
+        GrandChildShooterList[Player.DiceNum].ForEach(grandChildShooter => { grandChildShooter.GetComponent<ParticlePlayer>().enabled = true; });
+
+        GetComponent<ParticlePlayer>().enabled = true;
+        //ChildShooterList.ForEach(u => u.GetComponent<ParticlePlayer>().enabled = false);
+        //ChildShooterList[Player.DiceNum - 1].GetComponent<ParticlePlayer>().enabled = true;
     }
 
     public void GetChildShooter() {
         for (int i = 0; i < 6; i++) {
-            childShooterList.Add(transform.GetChild(i).gameObject);
+            ChildShooterList.Add(transform.GetChild(i).gameObject);
+        }
+    }
+
+    public void GetGrandChildShooterList() {
+        for (int i = 0; i < ChildShooterList.Count; i++) {
+            ChildShooterList.ForEach(childShooter => { GrandChildShooterList[i].Add(childShooter); });
         }
     }
 }
