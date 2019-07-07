a4cd28bd99443ad512d807a8113a0bf0762e571c
Naoki Aoyama <syrup3663ve@gmail.com>
2019-07-07 18:10:14 +0900

    Add Particle

--- Assets/Scripts/ObjectBase/Character/Player/Player.cs
+++ Assets/Scripts/ObjectBase/Character/Player/Player.cs
@@ -14,6 +14,7 @@ public class Player : Character {
     [SerializeField] private Dictionary<int, Vector3> diceEulerDict = new Dictionary<int, Vector3>() { { 1, new Vector3(90, -90, 0) }, { 2, new Vector3(-90, 0, 0) }, { 3, new Vector3(0, 180, 0) }, { 4, new Vector3(0, 0, 0) }, { 5, new Vector3(90, 0, 0) }, { 6, new Vector3(90, 90, 0) } };
     [SerializeField] private Dictionary<int, float> diceSpeedDict = new Dictionary<int, float>() { { 1, 5f }, { 2, 7.5f }, { 3,9.0f }, { 4, 11f }, { 5, 12.5f }, { 6, 15f } };
     [SerializeField] private GameObject playerShootActivator;
+    [SerializeField] private Camera playerCamera;
 
     public float Padding { get => padding; set => padding = value; }
     public GameObject DiceChild { get => diceChild; set => diceChild = value; }
@@ -45,8 +46,8 @@ public class Player : Character {
     }
 
     public void Clamp() {
-        var minPos = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
-        var maxPosd = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+        var minPos = Camera.main.ViewportToWorldPoint(new Vector2(0, -0));
+        var maxPosd = Camera.main.ViewportToWorldPoint(new Vector2(1f, 1));
         var newPos = transform.position;
         newPos.x = Mathf.Clamp(newPos.x, minPos.x + Padding, maxPosd.x - Padding);
         newPos.y = Mathf.Clamp(newPos.y, minPos.y +Padding, maxPosd.y - Padding);
